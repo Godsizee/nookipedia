54e66e4e8f1bf0edf18213b16a7b31624d520a54
Godsizee <badesebastian@outlook.com>
2026-03-21 23:34:11 +0100

    Update CreatureController.php

--- app/Controllers/CreatureController.php
+++ app/Controllers/CreatureController.php
@@ -12,18 +12,21 @@ class CreatureController {
     }
 
     public function insects() {
+        $title = 'Insekten';
         $category = 'insect';
         $creatures = $this->repo->getByCategory('insect');
         require __DIR__ . '/../../views/creature-list.php';
     }
 
     public function fish() {
+        $title = 'Fische';
         $category = 'fish';
         $creatures = $this->repo->getByCategory('fish');
         require __DIR__ . '/../../views/creature-list.php';
     }
 
     public function sea() {
+        $title = 'Meerestiere';
         $category = 'sea';
         $creatures = $this->repo->getByCategory('sea');
         require __DIR__ . '/../../views/creature-list.php';
