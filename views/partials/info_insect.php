aa5ae0b2618760f1275d0305bc2c972f045ef6e4
Godsizee <badesebastian@outlook.com>
2026-03-22 16:02:44 +0100

    Update info_insect.php

---
+++ views/partials/info_insect.php
@@ -0,0 +1,11 @@
+<h2>🦋 Die faszinierende Welt der Insekten</h2>
+<p>
+    Hier findest du eine vollständige und fabelhafte Übersicht aller 80 Insekten in <i>Animal Crossing: New Horizons</i>. 
+    Schnapp dir deinen <strong>Kescher</strong> und los geht's!
+</p>
+<p>
+    <strong>Deko-Tipp:</strong> Du kannst gefangene Insekten mit ihren schicken Tierbehältnissen auch wundervoll in deinem Haus oder überall auf deiner Insel platzieren.
+</p>
+<p>
+    <strong>Gut zu wissen:</strong> Bei Regen verkriechen sich die meisten Insekten und sind nicht anzutreffen. Ein kleiner Lifehack für die Jagd: Wenn du ein Gebäude betrittst und wieder verlässt, werden die Insekten draußen komplett neu geladen. Happy Bug Catching!
+</p>
