365bd0cde76fd8837054fd6e9e60332c16c01987
Godsizee <badesebastian@outlook.com>
2026-03-22 18:41:31 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -42,7 +42,8 @@ $router->add('insekten', 'CreatureController', 'insects');
 $router->add('fische', 'CreatureController', 'fish');
 $router->add('meerestiere', 'CreatureController', 'sea');
 $router->add('blumen', 'FlowerController', 'index');
-$router->add('blume', 'FlowerController', 'show');
+$router->add('blumen', 'FlowerController', 'index'); // Für die Übersicht
+$router->add('blume', 'FlowerController', 'show');   // WICHTIG: Für die Detailseite!
 
 // --- Route berechnen ---
 $route = $requestUri;
