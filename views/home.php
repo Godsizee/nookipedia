cee7c4190fbe01ca52e9cb7e75b9c2c345a32885
Godsizee <badesebastian@outlook.com>
2026-03-23 16:27:28 +0100

    1

--- views/home.php
+++ views/home.php
@@ -10,35 +10,30 @@ include 'partials/header.php';
 </section>
 
 <div class="category-selection">
-    <!-- Verzweigt zur Insekten-Seite, von wo aus auf Fische & Meerestiere navigiert werden kann -->
     <a href="/insekten" class="cat-card faunapaedie">
-        <i>🦉</i>
+        <i><img src="/assets/img/acnh/home/vogelfalter.png" alt="Faunapädie" style="max-width: 80%; max-height: 80%; object-fit: contain;"></i>
         <span>Faunapädie</span>
     </a>
     
-    <!-- Link für Blumen (aktuell noch ohne Route, bereitet aber den Ausbau vor) -->
     <a href="/blumen" class="cat-card flowers">
-        <i>🌷</i>
+        <i><img src="/assets/img/acnh/home/Red_Cosmos_NL_Icon.png" alt="Pflanzenwelt" style="max-width: 80%; max-height: 80%; object-fit: contain;"></i>
         <span>Pflanzenwelt</span>
     </a>
 
-
-    <!-- NEU: Link für Feiertage & Events -->
     <a href="/events" class="cat-card events">
-        <i>🎈</i>
+        <i><img src="/assets/img/acnh/home/64px-Red_Balloon_NH_Icon.png" alt="Feiertage & Events" style="max-width: 80%; max-height: 80%; object-fit: contain;"></i>
         <span>Feiertage & Events</span>
     </a>
 
-        <a href="/materialien" class="cat-card" style="border-color: rgba(41, 182, 246, 0.4);">
-        <i>🪓</i>
+    <a href="/materialien" class="cat-card" style="border-color: rgba(41, 182, 246, 0.4);">
+        <i><img src="/assets/img/acnh/home/64px-Tree_Branch_NH_Inv_Icon.png" alt="Materialien & Zutaten" style="max-width: 80%; max-height: 80%; object-fit: contain;"></i>
         <span>Materialien & Zutaten</span>
     </a>
 
-        <a href="/bastelanleitungen" class="cat-card" style="border-color: rgba(255, 152, 0, 0.4);">
-        <i>🛠️🍳</i>
+    <a href="/bastelanleitungen" class="cat-card" style="border-color: rgba(255, 152, 0, 0.4);">
+        <i><img src="/assets/img/acnh/home/BastelnKochen.png" alt="Bastelanleitungen & Kochrezepte" style="max-width: 80%; max-height: 80%; object-fit: contain;"></i>
         <span>Bastelanleitungen & Kochrezepte</span>
     </a>
-    
 </div>
 
 <div style="margin-top: 40px; text-align: center;">
