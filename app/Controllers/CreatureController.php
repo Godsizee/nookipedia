291a209ffa5627ed21bea37e7b5c15e5f0bc83d6
Godsizee <badesebastian@outlook.com>
2026-04-20 13:23:25 +0200

    Update CreatureController.php

--- app/Controllers/CreatureController.php
+++ app/Controllers/CreatureController.php
@@ -5,34 +5,57 @@ namespace App\Controllers;
 use App\Repositories\CreatureRepository;
 
 class CreatureController {
-    private $repo;
+    
+    private $creatureRepo;
 
     public function __construct() {
-        $this->repo = new CreatureRepository();
+        // Repository initialisieren, damit wir auf die Datenbank zugreifen können
+        $this->creatureRepo = new CreatureRepository();
     }
 
-    public function insects() {
-        $title = 'Insekten';
-        $category = 'insect';
-        // Definiere das spezifische Fluff-Text Partial (OCP & SRP)
-        $infoTemplate = 'partials/info_insect.php';
-        $creatures = $this->repo->getByCategory('insect');
-        require __DIR__ . '/../../views/creature-list.php';
-    }
-
-    public function fish() {
+    /**
+     * Übersicht: Fische
+     */
+    public function fische() {
+        $creatures = $this->creatureRepo->findByCategory('fish');
         $title = 'Fische';
-        $category = 'fish';
-        $infoTemplate = 'partials/info_fish.php';
-        $creatures = $this->repo->getByCategory('fish');
-        require __DIR__ . '/../../views/creature-list.php';
+        require __DIR__ . '/../../views/faunapedia-fish.php';
     }
 
-    public function sea() {
+    /**
+     * Übersicht: Insekten
+     */
+    public function insekten() {
+        $creatures = $this->creatureRepo->findByCategory('insect');
+        $title = 'Insekten';
+        require __DIR__ . '/../../views/faunapedia-insects.php';
+    }
+
+    /**
+     * Übersicht: Meerestiere
+     */
+    public function meerestiere() {
+        $creatures = $this->creatureRepo->findByCategory('sea');
         $title = 'Meerestiere';
-        $category = 'sea';
-        $infoTemplate = 'partials/info_sea.php';
-        $creatures = $this->repo->getByCategory('sea');
-        require __DIR__ . '/../../views/creature-list.php';
+        require __DIR__ . '/../../views/faunapedia-sea.php';
+    }
+
+    /**
+     * NEU: Dynamische Detailseite für ein einzelnes Tier
+     * Wird über die Route /tier/{id} aufgerufen
+     */
+    public function show($id) {
+        // 1. Lade das Tier aus der Datenbank anhand der ID
+        $creature = $this->creatureRepo->findById($id);
+        
+        // 2. Sicherheitscheck: Wenn die ID nicht existiert, zurück zur Startseite leiten
+        if (!$creature) {
+            header("Location: /");
+            exit;
+        }
+
+        // 3. Titel setzen und die fabelhafte Detail-View laden
+        $title = $creature->name . ' - Faunapädie';
+        require __DIR__ . '/../../views/creature-detail.php';
     }
 }
